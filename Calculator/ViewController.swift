9665311c97ea4080674315b790a6524f0926b30b
Steve Lederer <stevieled@gmail.com>
2018-12-24 18:53:39 -0500

    Initial Commit

---
+++ Calculator/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  Calculator
+//
+//  Created by Steve Lederer on 12/24/18.
+//  Copyright © 2018 Steve Lederer. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+
+}
+
